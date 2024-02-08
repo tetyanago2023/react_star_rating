97c4fa09dcf36efe4973ab85cc4c7803153648c4
go2023 <tetyanago2018@gmail.com>
2024-02-07 16:07:56 -0800

    StarRating component was completed

--- src/components/star-rating/index.jsx
+++ src/components/star-rating/index.jsx
@@ -8,26 +8,30 @@ export default function StarRating ({ numberOfStars = 5}) {
 
     const handleClick = (getCurrentIndex) => {
         console.log(getCurrentIndex)
+        setRating(getCurrentIndex)
     }
 
     const handleMouseMove = (getCurrentIndex) => {
         console.log(getCurrentIndex)
+        setHover(getCurrentIndex)
     }
 
     const handleMouseLeave = (getCurrentIndex) => {
         console.log(getCurrentIndex)
+        setHover(rating)
     }
     return (
         <div className="star-rating">
-            {[...Array(numberOfStars)].map((star, index) => {
-                const getCurrentIndex = index + 1;
+            {[...Array(numberOfStars)].map((_, index) => {
+                index += 1;
                 return <
                     FaStar
                     key={index}
-                    className="star"
-                    onClick={() => handleClick(getCurrentIndex)}
-                    onMouseMove={() => handleMouseMove(getCurrentIndex)}
-                    onMouseLeave={() => handleMouseLeave(getCurrentIndex)}
+                    className={index <= (hover || rating) ? "active" : "inactive"}
+                    onClick={() => handleClick(index)}
+                    onMouseMove={() => handleMouseMove(index)}
+                    onMouseLeave={() => handleMouseLeave()}
+                    size={40}
                 />
             })}
         </div>
